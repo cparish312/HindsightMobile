69001ab9686154cd076f04b0ea43018195343e92
cparish312 <54302513+cparish312@users.noreply.github.com>
2024-11-21 11:13:56 -0500

    CrashLog file

--- app/src/main/java/com/connor/hindsightmobile/App.kt
+++ app/src/main/java/com/connor/hindsightmobile/App.kt
@@ -5,6 +5,10 @@ import com.connor.hindsightmobile.obj.ObjectBoxStore
 import com.connor.hindsightmobile.utils.NotificationHelper
 import com.connor.hindsightmobile.utils.Preferences
 import com.connor.llamacpp.LlamaCpp
+import java.io.File
+import java.io.FileOutputStream
+import java.io.PrintWriter
+import java.util.Date
 
 class App : Application() {
     val llamaCpp = LlamaCpp()
@@ -15,5 +19,24 @@ class App : Application() {
         NotificationHelper.buildNotificationChannels(this)
         ObjectBoxStore.init(this)
         llamaCpp.init()
+
+        // Create a crash log file
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            logCrashToFile(throwable)
+            Thread.getDefaultUncaughtExceptionHandler()?.uncaughtException(thread, throwable)
+        }
+    }
+
+    private fun logCrashToFile(throwable: Throwable) {
+        try {
+            val logFile = File(getExternalFilesDir(null), "crash_logs.txt")
+            val logWriter = PrintWriter(FileOutputStream(logFile, true))
+            logWriter.println("Crash Log: ${Date()}")
+            throwable.printStackTrace(logWriter)
+            logWriter.println()
+            logWriter.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
