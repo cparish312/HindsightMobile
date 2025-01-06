281fc106e2b43bcd7cfde7de6d53977042cf8cf8
cparish312 <54302513+cparish312@users.noreply.github.com>
2025-01-06 15:11:55 -0500

    remove logs

--- app/src/main/java/com/connor/hindsightmobile/ui/elements/Messages.kt
+++ app/src/main/java/com/connor/hindsightmobile/ui/elements/Messages.kt
@@ -1,6 +1,5 @@
 package com.connor.hindsightmobile.ui.elements
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
